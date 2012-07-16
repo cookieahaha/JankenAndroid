1f2e88fa8299c961430f80adb1a360b98abcbbc9
Yasukazu Kashii <cookieahaha@gmail.com>
2012-07-15 19:11:27 -0700

    nhtgisurngsrjni

---
+++ src/me/kukkii/janken/TimerThread.java
@@ -0,0 +1,39 @@
+package me.kukkii.janken;
+
+public class TimerThread implements Runnable{
+
+  private Janken janken;
+
+  public TimerThread(Janken janken){
+    this.janken = janken;
+  }
+
+  public void run(){
+    try{
+      Thread.sleep(3000);
+    }
+    catch (InterruptedException e){
+    }   
+    janken.jan();
+    try{
+      Thread.sleep(500);
+    }
+    catch (InterruptedException e){
+    }   
+    janken.ken();
+    try{
+      Thread.sleep(500);
+    }
+    catch (InterruptedException e){
+    }   
+    janken.pon();
+    try{
+      Thread.sleep(50);
+    }
+    catch (InterruptedException e){
+    }   
+    janken.afterPon();
+  }
+
+
+}
