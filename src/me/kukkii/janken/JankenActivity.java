71e42cb53b20d42fe4149b85825d5f519b003444
Yasukazu Kashii <cookieahaha@gmail.com>
2012-08-03 20:30:01 -0700

    sound

--- src/me/kukkii/janken/JankenActivity.java
+++ src/me/kukkii/janken/JankenActivity.java
@@ -18,7 +18,10 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.TextView;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
 
 import com.google.ads.*;
 
@@ -31,29 +34,34 @@ public class JankenActivity extends Activity {
   private GameManager gameManager;
   private boolean resumed;
 
+  private MediaPlayer mp;
+  private SoundPool soundPool;
+  private int soundID;
+  boolean loaded = false;
+  
   public void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
     AdView adView = (AdView)this.findViewById(R.id.adView1);
     adView.loadAd(new AdRequest());
+
+    setVolumeControlStream(AudioManager.STREAM_MUSIC);
     
-    MediaPlayer player = new MediaPlayer();
-    try {
-      player.setDataSource("R.raw.bgm1.mp3");
-      player.setLooping(true);
-      player.prepare();
-      player.start();
-    } 
-    catch (IllegalArgumentException e) {
-      e.printStackTrace();
-    } 
-    catch (IllegalStateException e) {
-      e.printStackTrace();
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
+    soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+    soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+      @Override
+      public void onLoadComplete(SoundPool soundPool, int sampleId,
+          int status) {
+        loaded = true;
+      }
+    });
+    soundID = soundPool.load(this, R.raw.janken, 1);
+    
+    mp = MediaPlayer.create(this, R.raw.bgm1);
+    mp.setLooping(true);
+    mp.start();
+    mp.setVolume(0.3F, 0.3F);
   }
 
   public void onStart(){
@@ -66,12 +74,17 @@ public class JankenActivity extends Activity {
   protected void onStop() {
     super.onStop();
     dataManager.close();
+    mp.stop();
     Log.i(tag, "dataManager was closed");
   }
 
   public void onResume(){
     super.onResume();
     resumed = true;
+    
+    SoundPool soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);  
+    int soundID = soundPool.load(this, R.raw.bgm1, 1); 
+    soundPool.play(soundID, 1.0F, 1.0F, 1, -1, 1.0F); 
 
     gameManager = new GameManager(this, dataManager);
   }
@@ -111,14 +124,50 @@ public class JankenActivity extends Activity {
 
   public void showJan(){
     showMessage("Jan");
+    
+    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+    float actualVolume = (float) audioManager
+        .getStreamVolume(AudioManager.STREAM_MUSIC);
+    float maxVolume = (float) audioManager
+        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+    float volume = actualVolume / maxVolume;
+    // Is the sound loaded already?
+    if (loaded) {
+      soundPool.play(soundID, volume, volume, 1, 0, 1f);
+      Log.e("Test", "Played sound");
+    }
   }
 
   public void showKen(){
     showMessage("Ken");
+
+    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+    float actualVolume = (float) audioManager
+        .getStreamVolume(AudioManager.STREAM_MUSIC);
+    float maxVolume = (float) audioManager
+        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+    float volume = actualVolume / maxVolume;
+    // Is the sound loaded already?
+    if (loaded) {
+      soundPool.play(soundID, volume, volume, 1, 0, 1f);
+      Log.e("Test", "Played sound");
+    }
   }
     
   public void showPon(){
     showMessage("Pon");
+
+    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+    float actualVolume = (float) audioManager
+        .getStreamVolume(AudioManager.STREAM_MUSIC);
+    float maxVolume = (float) audioManager
+        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+    float volume = actualVolume / maxVolume;
+    // Is the sound loaded already?
+    if (loaded) {
+      soundPool.play(soundID, volume, volume, 1, 0, 1f);
+      Log.e("Test", "Played sound");
+    }
   }
 
   public void onClickHand(View view){
