728dc72d7df2716882cdc29e49fb426272f83597
Yasukazu Kashii <cookieahaha@gmail.com>
2012-08-13 19:40:58 -0700

    fragment

--- src/me/kukkii/janken/AdFragment.java
+++ src/me/kukkii/janken/AdFragment.java
@@ -2,6 +2,9 @@ package me.kukkii.janken;
 
 import android.app.Activity;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.os.Bundle;
 
 import com.google.ads.AdRequest;
@@ -9,11 +12,12 @@ import com.google.ads.AdView;
 
 public class AdFragment extends Fragment{
 
-  public void onCreate(Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {    super.onCreate(savedInstanceState);
     Activity activity = getActivity();
-    AdView adView = (AdView)activity.findViewById(R.id.adView1);
+    AdView adView = (AdView)activity.findViewById(R.id.adView);
     adView.loadAd(new AdRequest());
+    return inflater.inflate(R.layout.ad_fragment, container, false);
+
   }
 
 }
